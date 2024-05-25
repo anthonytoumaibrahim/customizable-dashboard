5b9a236d31c79c9cfcd4d23b63c4c864b4e65564
atian155 <atianthony@outlook.com>
2024-05-25 20:16:17 +0300

    Add HEX to RGBA convert function

--- resources/js/Pages/Dashboard/components/Widgets/Charts/DoughnutChart/index.tsx
+++ resources/js/Pages/Dashboard/components/Widgets/Charts/DoughnutChart/index.tsx
@@ -7,6 +7,7 @@ interface DoughnutChartProps {
 
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
+import { hexToRgba } from "@/bootstrap";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -16,6 +17,9 @@ const DoughnutChart = ({
     color1 = "#6366f1",
     color2 = "#1f2937",
 }: DoughnutChartProps) => {
+    const rgbaColor1 = hexToRgba(color1, 0.2);
+    const rgbaColor2 = hexToRgba(color2, 0.2);
+
     const data = {
         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
         datasets: [
@@ -23,16 +27,16 @@ const DoughnutChart = ({
                 label: "# of Votes",
                 data: [12, 19, 3, 5, 2, 3],
                 backgroundColor: [
-                    "rgba(255, 99, 132, 0.2)",
-                    "rgba(54, 162, 235, 0.2)",
+                    rgbaColor1,
+                    rgbaColor2,
                     "rgba(255, 206, 86, 0.2)",
                     "rgba(75, 192, 192, 0.2)",
                     "rgba(153, 102, 255, 0.2)",
                     "rgba(255, 159, 64, 0.2)",
                 ],
                 borderColor: [
-                    "rgba(255, 99, 132, 1)",
-                    "rgba(54, 162, 235, 1)",
+                    color1,
+                    color2,
                     "rgba(255, 206, 86, 1)",
                     "rgba(75, 192, 192, 1)",
                     "rgba(153, 102, 255, 1)",
--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,4 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+export const hexToRgba = (hex: string, alpha: number) => {
+    const bigint = parseInt(hex.slice(1), 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+};
