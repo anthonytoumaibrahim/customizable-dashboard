cef44fd4b6448423904bdea2abfac6443a26863a
atian155 <atianthony@outlook.com>
2024-05-25 18:53:25 +0300

    Add Charts Data

---
+++ resources/js/Pages/Dashboard/components/NewWidgetModal/ChartsWidgetCreator/charts.ts
@@ -0,0 +1,7 @@
+export const charts = [
+    {
+        id: 1,
+        name: "Vertical Bar Chart",
+        image: "/assets/widgets/vertical.svg",
+    },
+];
--- resources/js/Pages/Dashboard/components/NewWidgetModal/ChartsWidgetCreator/index.tsx
+++ resources/js/Pages/Dashboard/components/NewWidgetModal/ChartsWidgetCreator/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import VerticalBarChart from "../../Widgets/Charts/VerticalBarChart";
+import { charts } from "./charts";
 
 const ChartsWidgetCreator = () => {
     const [selectedChart, setSelectedChart] = useState<boolean | null>(null);
@@ -8,7 +8,26 @@ const ChartsWidgetCreator = () => {
         "selected!"
     ) : (
         <div className="grid grid-cols-2 gap-4">
-            <div></div>
+            {charts.map((chart) => {
+                const { id, name, image } = chart;
+                return (
+                    <div
+                        key={id}
+                        className="rounded border dark:border-gray-900 shadow-sm overflow-hidden cursor-pointer group hover:border-indigo-500 transition-colors duration-150"
+                    >
+                        <div className="p-4 dark:bg-gray-900 group-hover:bg-indigo-100 transition-colors duration-150">
+                            <img
+                                src={image}
+                                alt=""
+                                className="object-contain"
+                            />
+                        </div>
+                        <div className="p-4 bg-gray-50 dark:bg-gray-950 text-center group-hover:bg-indigo-500 group-hover:text-white transition-colors duration-150">
+                            <h4 className="text-lg font-bold">{name}</h4>
+                        </div>
+                    </div>
+                );
+            })}
         </div>
     );
 };
