16d97691e568750c5fd6a2721b117e8e1831a787
atian155 <atianthony@outlook.com>
2024-05-25 11:28:02 +0300

    Add Tab Button Styling and Data

--- resources/js/Pages/Dashboard/components/NewWidgetModal/index.tsx
+++ resources/js/Pages/Dashboard/components/NewWidgetModal/index.tsx
@@ -15,28 +15,34 @@ const NewWidgetModal = ({
     return (
         <Modal show={isOpen} onClose={handleClose} maxWidth="2xl">
             <Tab.Group vertical>
-                <Tab.List className="flex flex-col">
-                    {tabs.map((tab, tabIndex) => (
-                        <Tab as={Fragment} key={tabIndex}>
-                            {({ selected }) => (
-                                <button
-                                    className={
-                                        selected
-                                            ? "bg-blue-500 text-white"
-                                            : "bg-white text-black"
-                                    }
-                                >
-                                    {tab.name}
-                                </button>
-                            )}
-                        </Tab>
-                    ))}
-                </Tab.List>
-                <Tab.Panels>
-                    <Tab.Panel>Content 1</Tab.Panel>
-                    <Tab.Panel>Content 2</Tab.Panel>
-                    <Tab.Panel>Content 3</Tab.Panel>
-                </Tab.Panels>
+                <div className="flex gap-4 min-h-[320px]">
+                    <Tab.List className="flex flex-col w-1/3 bg-gray-100">
+                        {tabs.map((tab, tabIndex) => {
+                            const { name, icon: TabIcon } = tab;
+                            return (
+                                <Tab as={Fragment} key={tabIndex}>
+                                    {({ selected }) => (
+                                        <button
+                                            className={`flex items-center justify-center gap-2 p-4 ${
+                                                selected
+                                                    ? "bg-indigo-500 text-white"
+                                                    : "hover:bg-indigo-200 transition-colors duration-150"
+                                            }`}
+                                        >
+                                            <TabIcon size={24} />
+                                            {tab.name}
+                                        </button>
+                                    )}
+                                </Tab>
+                            );
+                        })}
+                    </Tab.List>
+                    <Tab.Panels className="p-4">
+                        <Tab.Panel>Content 1</Tab.Panel>
+                        <Tab.Panel>Content 2</Tab.Panel>
+                        <Tab.Panel>Content 3</Tab.Panel>
+                    </Tab.Panels>
+                </div>
             </Tab.Group>
         </Modal>
     );
--- resources/js/Pages/Dashboard/data.ts
+++ resources/js/Pages/Dashboard/data.ts
@@ -1,11 +1,16 @@
+import { FaChartPie, FaSpotify, FaCloud } from "react-icons/fa6";
+
 export const tabs = [
     {
         name: "Charts",
+        icon: FaChartPie,
     },
     {
         name: "Spotify",
+        icon: FaSpotify,
     },
     {
         name: "Weather",
+        icon: FaCloud,
     },
 ];
