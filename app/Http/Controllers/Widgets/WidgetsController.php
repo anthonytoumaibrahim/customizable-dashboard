31c209cc533488f479e1eb07b385d5c0f7fcd483
atian155 <atianthony@outlook.com>
2024-05-25 20:22:46 +0300

    Add Widgets Controller

---
+++ app/Http/Controllers/Widgets/WidgetsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Widgets;
+
+use App\Http\Controllers\Controller;
+use App\Models\Widget;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
+class WidgetsController extends Controller
+{
+    public function addWidget(Request $request)
+    {
+        $widget = new Widget();
+        $widget->widget_id = $request->widget_id;
+        $widget->user_id = $request->user()->id;
+        $widget->name = $request->name;
+        $widget->color1 = $request->color1;
+        $widget->color2 = $request->color2;
+        $widget->dataset_url = $request->dataset_url;
+        $widget->widget_data = $request->widget_data;
+        $widget->saveOrFail();
+
+        return Redirect::route('dashboard');
+    }
+}
