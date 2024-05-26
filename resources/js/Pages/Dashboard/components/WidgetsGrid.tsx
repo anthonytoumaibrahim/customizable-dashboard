918eb09092eede6c3fddcd0e139b268f78957448
atian155 <atianthony@outlook.com>
2024-05-26 18:26:42 +0300

    Add Widgets Selector to Grid

--- resources/js/Pages/Dashboard/components/WidgetsGrid.tsx
+++ resources/js/Pages/Dashboard/components/WidgetsGrid.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { WidgetsType } from "..";
 import { widgets as widgetsData } from "./Widgets/widgets";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 
 interface WidgetsGridProps {
     widgets: Array<WidgetsType>;
@@ -25,15 +26,24 @@ import {
 import SortableItem from "./SortableItem";
 
 const WidgetsGrid = ({ widgets = [] }: WidgetsGridProps) => {
-    const [items, setItems] = useState(
-        widgets.sort((a, b) => a.order - b.order)
+    const widgetsSelector = useAppSelector(
+        (state) => state.widgetsSlice.widgets
     );
+    const dispatch = useAppDispatch();
+
     const sensors = useSensors(
         useSensor(PointerSensor),
         useSensor(KeyboardSensor, {
             coordinateGetter: sortableKeyboardCoordinates,
         })
     );
+
+    useEffect(() => {
+        dispatch({
+            type: "widgets/initializeWidgets",
+            payload: widgets.sort((a, b) => a.order - b.order),
+        });
+    }, []);
 
     function handleDragEnd(event: DragEndEvent) {
         const { active, over } = event;
@@ -43,55 +53,75 @@ const WidgetsGrid = ({ widgets = [] }: WidgetsGridProps) => {
         }
 
         if (active.id !== over.id) {
-            setItems((items) => {
-                const activeIndex = items.findIndex(
-                    (item) => item.id === active.id
-                );
-                const overIndex = items.findIndex(
-                    (item) => item.id === over.id
-                );
+            const activeIndex = widgetsSelector.findIndex(
+                (item) => item.id === active.id
+            );
+            const overIndex = widgetsSelector.findIndex(
+                (item) => item.id === over.id
+            );
 
-                const newItems = [...items];
-                const tempOrder = newItems[activeIndex].order;
-                newItems[activeIndex].order = newItems[overIndex].order;
-                newItems[overIndex].order = tempOrder;
+            const newItems = widgetsSelector.map((item, index) => {
+                if (index === activeIndex) {
+                    return { ...item, order: widgetsSelector[overIndex].order };
+                } else if (index === overIndex) {
+                    return {
+                        ...item,
+                        order: widgetsSelector[activeIndex].order,
+                    };
+                } else {
+                    return item;
+                }
+            });
 
-                return newItems.sort((a, b) => a.order - b.order);
+            dispatch({
+                type: "widgets/initializeWidgets",
+                payload: newItems.sort((a, b) => a.order - b.order),
             });
         }
     }
 
     return (
-        <DndContext
-            sensors={sensors}
-            collisionDetection={closestCenter}
-            onDragEnd={handleDragEnd}
-        >
-            <SortableContext items={items} strategy={rectSwappingStrategy}>
-                <div className="grid grid-cols-3 gap-6 mt-6">
-                    {items.map((data) => {
-                        const { id, name, type, widget_id, color1, color2 } =
-                            data;
-                        const widget = widgetsData?.[type].filter(
-                            (widget) => widget.id === widget_id
-                        )?.[0];
-                        const WidgetComponent = widget.component;
+        widgetsSelector.length > 0 && (
+            <DndContext
+                sensors={sensors}
+                collisionDetection={closestCenter}
+                onDragEnd={handleDragEnd}
+            >
+                <SortableContext
+                    items={widgetsSelector}
+                    strategy={rectSwappingStrategy}
+                >
+                    <div className="grid grid-cols-3 gap-6 mt-6">
+                        {widgetsSelector.map((data) => {
+                            const {
+                                id,
+                                name,
+                                type,
+                                widget_id,
+                                color1,
+                                color2,
+                            } = data;
+                            const widget = widgetsData?.[type].filter(
+                                (widget) => widget.id === widget_id
+                            )?.[0];
+                            const WidgetComponent = widget.component;
 
-                        return (
-                            <SortableItem key={id} id={id}>
-                                <div className="h-80 p-6 rounded-lg bg-white dark:bg-gray-950 border border-gray-200 dark:border-gray-900 grid place-content-center shadow hover:shadow-lg">
-                                    <WidgetComponent
-                                        name={name}
-                                        color1={color1}
-                                        color2={color2}
-                                    />
-                                </div>
-                            </SortableItem>
-                        );
-                    })}
-                </div>
-            </SortableContext>
-        </DndContext>
+                            return (
+                                <SortableItem key={id} id={id}>
+                                    <div className="h-80 p-6 rounded-lg bg-white dark:bg-gray-950 border border-gray-200 dark:border-gray-900 grid place-content-center shadow hover:shadow-lg">
+                                        <WidgetComponent
+                                            name={name}
+                                            color1={color1}
+                                            color2={color2}
+                                        />
+                                    </div>
+                                </SortableItem>
+                            );
+                        })}
+                    </div>
+                </SortableContext>
+            </DndContext>
+        )
     );
 };
 
