e92e8f9165c695fa0564be49f8168381135e3dde
atian155 <atianthony@outlook.com>
2024-05-26 22:51:58 +0300

    Improve Widget Creator Responsiveness

--- resources/js/Pages/Dashboard/components/NewWidgetModal/index.tsx
+++ resources/js/Pages/Dashboard/components/NewWidgetModal/index.tsx
@@ -17,21 +17,24 @@ const NewWidgetModal = ({
     return (
         <Modal show={isOpen} onClose={handleClose} maxWidth="2xl">
             <Tab.Group vertical>
-                <div className="flex gap-4 min-h-[320px]">
-                    <Tab.List className="flex flex-col w-max bg-gray-100 dark:bg-gray-950">
+                <div className="flex max-md:flex-col gap-4 md:min-h-[320px]">
+                    <Tab.List className="flex md:flex-col w-full md:w-max bg-gray-100 dark:bg-gray-950 max-md:overflow-auto">
                         {tabs.map((tab, tabIndex) => {
                             const { name, icon: TabIcon } = tab;
                             return (
                                 <Tab as={Fragment} key={tabIndex}>
                                     {({ selected }) => (
                                         <button
-                                            className={`flex items-center gap-2 py-4 px-10 ${
+                                            className={`flex items-center gap-2 p-4 md:py-4 md:px-10 ${
                                                 selected
                                                     ? "bg-indigo-500 text-white"
                                                     : "hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-150"
                                             }`}
                                         >
-                                            <TabIcon size={24} />
+                                            <TabIcon
+                                                size={24}
+                                                className="shrink-0"
+                                            />
                                             {tab.name}
                                         </button>
                                     )}
